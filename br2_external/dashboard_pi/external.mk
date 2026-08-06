335a3d8b911c7acc314a4fd6f3131482c57e9c91
Răzvan Corneliu C.R. VILT <16434603+d3vi1@users.noreply.github.com.>
2026-08-06 11:46:44 +0300

    media: enable GStreamer GL on direct GBM

--- br2_external/dashboard_pi/external.mk
+++ br2_external/dashboard_pi/external.mk
@@ -1,1 +1,9 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_DASHBOARD_PI_PATH)/package/*/*.mk))
+
+# Buildroot 2026.05 does not expose GStreamer's direct EGL/GBM winsys in
+# gst1-plugins-base. WPEPlatform DRM needs gstreamer-gl without X11/Wayland.
+ifeq ($(BR2_PACKAGE_DASHBOARD_PI_WPEWEBKIT),y)
+GST1_PLUGINS_BASE_CONF_OPTS := $(filter-out -Dgl=disabled -Dgl_winsys=%,$(GST1_PLUGINS_BASE_CONF_OPTS))
+GST1_PLUGINS_BASE_CONF_OPTS += -Dgl=enabled -Dgl_winsys=gbm
+GST1_PLUGINS_BASE_DEPENDENCIES += libgbm libdrm libgudev
+endif
